a404e655023294dd45da5a2fe3a1312b82abf42c
rohanms888 <88832105+rohanms888@users.noreply.github.com>
2023-05-11 11:08:37 +0530

    Update auth.js

---
+++ reducers/auth.js
@@ -0,0 +1,42 @@
+import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from '../constants/authConstants';
+
+const initialState = {
+  loading: false,
+  isAuthenticated: false,
+  user: null,
+  error: null,
+};
+
+export const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: true,
+        user: action.payload,
+        error: null,
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: false,
+        user: null,
+        error: action.payload,
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
